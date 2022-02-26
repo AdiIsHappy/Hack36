ef18f46a74ea1da99cad7d936cdc9b72ae72a224
Aditya <b21083@students.iitmandi.ac.in>
2022-02-26 13:58:29 +0530

    Added List working

--- TaskV.js
+++ TaskV.js
@@ -1,17 +1,34 @@
 const infoPopup = document.getElementById("info_Popup")
 const overlay = document.getElementById("overlay")
+const taskLists = document.getElementsByClassName("TaksList")
 
 
-console.log("something");
-
-function openInfoPopup (){
+showTasks()
+function openInfoPopup() {
     infoPopup.classList.add("active")
     overlay.classList.add("active")
 }
-function closeInfoPopup (){
+function closeInfoPopup() {
     infoPopup.classList.remove("active")
     overlay.classList.remove("active")
 }
+
+function showTasks() {
+    for (let i = 0; i < taskLists.length; i++) {
+        let getLocalStorageData = localStorage.getItem(taskLists[i].id);
+        if (getLocalStorageData == null) {
+            listArray = [];
+        } else {
+            listArray = JSON.parse(getLocalStorageData);
+        }
+        let newLiTag = "";
+        listArray.forEach((element, index) => {
+            newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
+        });
+        taskLists[i].innerHTML = newLiTag;
+    }
+}
+
 
 
 
